9bf1621ca1636332dec0e59f9b1cc349ea7fba2d
dzzhan <dzzhan@163.com>
2018-11-19 19:52:04 +0800

    Add new file

---
+++ GraphEdior/TaskInfo.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace GraphEdior
+{
+    class TaskInfo
+    {
+        private int m_X;
+        private int m_Y;
+        private int m_W;
+        private int m_H;
+     
+        public TaskInfo(Point start, Point end)
+        {
+            m_X = Math.Min(start.X, end.X);
+            m_Y = Math.Min(start.Y, end.Y);
+            m_W = Math.Abs(start.X - end.X);
+            m_H = Math.Abs(start.Y - end.Y);
+        }
+
+        public int X
+        {
+            get { return m_X; }
+        }
+
+        public int Y
+        {
+            get { return m_Y; }
+        }
+
+        public int W
+        {
+            get { return m_W; }
+        }
+
+        public int H
+        {
+            get { return m_H; }
+        }
+    };
+
+}
