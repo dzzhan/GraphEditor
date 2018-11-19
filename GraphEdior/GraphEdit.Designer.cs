93ea5fd70e57c9c0b0b22419dfb815e63d1714e7
dzzhan <dzzhan@163.com>
2018-11-19 19:51:23 +0800

    commit comments

--- GraphEdior/GraphEdit.Designer.cs
+++ GraphEdior/GraphEdit.Designer.cs
@@ -30,11 +30,29 @@
         {
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.statusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.menuStrip2 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addDSPTaskToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addDependenceToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addSubmitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exportGraphDataToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadGraphDataToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.statusStrip1.SuspendLayout();
+            this.menuStrip2.SuspendLayout();
             this.SuspendLayout();
             // 
             // menuStrip1
             // 
-            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Location = new System.Drawing.Point(0, 25);
             this.menuStrip1.Name = "menuStrip1";
             this.menuStrip1.Size = new System.Drawing.Size(849, 24);
             this.menuStrip1.TabIndex = 0;
@@ -42,31 +60,176 @@
             // 
             // statusStrip1
             // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.statusLabel});
             this.statusStrip1.Location = new System.Drawing.Point(0, 497);
             this.statusStrip1.Name = "statusStrip1";
             this.statusStrip1.Size = new System.Drawing.Size(849, 22);
             this.statusStrip1.TabIndex = 1;
             this.statusStrip1.Text = "statusStrip1";
             // 
-            // Form1
+            // statusLabel
+            // 
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(44, 17);
+            this.statusLabel.Text = "Ready";
+            // 
+            // menuStrip2
+            // 
+            this.menuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.editToolStripMenuItem,
+            this.toolsToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip2.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip2.Name = "menuStrip2";
+            this.menuStrip2.Size = new System.Drawing.Size(849, 25);
+            this.menuStrip2.TabIndex = 2;
+            this.menuStrip2.Text = "menuStrip2";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem,
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(39, 21);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(108, 22);
+            this.newToolStripMenuItem.Text = "New";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(108, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(108, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addDSPTaskToolStripMenuItem,
+            this.addDependenceToolStripMenuItem,
+            this.addSubmitToolStripMenuItem});
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(42, 21);
+            this.editToolStripMenuItem.Text = "Edit";
+            // 
+            // addDSPTaskToolStripMenuItem
+            // 
+            this.addDSPTaskToolStripMenuItem.Name = "addDSPTaskToolStripMenuItem";
+            this.addDSPTaskToolStripMenuItem.Size = new System.Drawing.Size(177, 22);
+            this.addDSPTaskToolStripMenuItem.Text = "Add Task";
+            this.addDSPTaskToolStripMenuItem.Click += new System.EventHandler(this.addDSPTaskToolStripMenuItem_Click);
+            // 
+            // addDependenceToolStripMenuItem
+            // 
+            this.addDependenceToolStripMenuItem.Name = "addDependenceToolStripMenuItem";
+            this.addDependenceToolStripMenuItem.Size = new System.Drawing.Size(177, 22);
+            this.addDependenceToolStripMenuItem.Text = "Add Dependence";
+            // 
+            // addSubmitToolStripMenuItem
+            // 
+            this.addSubmitToolStripMenuItem.Name = "addSubmitToolStripMenuItem";
+            this.addSubmitToolStripMenuItem.Size = new System.Drawing.Size(177, 22);
+            this.addSubmitToolStripMenuItem.Text = "Add Submit";
+            // 
+            // toolsToolStripMenuItem
+            // 
+            this.toolsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exportGraphDataToolStripMenuItem,
+            this.loadGraphDataToolStripMenuItem});
+            this.toolsToolStripMenuItem.Name = "toolsToolStripMenuItem";
+            this.toolsToolStripMenuItem.Size = new System.Drawing.Size(52, 21);
+            this.toolsToolStripMenuItem.Text = "Tools";
+            // 
+            // exportGraphDataToolStripMenuItem
+            // 
+            this.exportGraphDataToolStripMenuItem.Name = "exportGraphDataToolStripMenuItem";
+            this.exportGraphDataToolStripMenuItem.Size = new System.Drawing.Size(185, 22);
+            this.exportGraphDataToolStripMenuItem.Text = "Export Graph Data";
+            // 
+            // loadGraphDataToolStripMenuItem
+            // 
+            this.loadGraphDataToolStripMenuItem.Name = "loadGraphDataToolStripMenuItem";
+            this.loadGraphDataToolStripMenuItem.Size = new System.Drawing.Size(185, 22);
+            this.loadGraphDataToolStripMenuItem.Text = "Load Graph Data";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(47, 21);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(111, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // GraphEdit
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
             this.ClientSize = new System.Drawing.Size(849, 519);
             this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.menuStrip2);
             this.MainMenuStrip = this.menuStrip1;
-            this.Name = "Form1";
+            this.Name = "GraphEdit";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.GraphEdit_Load);
+            this.SizeChanged += new System.EventHandler(this.GraphEdit_Resize);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.GraphEdit_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.GraphEdit_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.GraphEdit_MouseUp);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.menuStrip2.ResumeLayout(false);
+            this.menuStrip2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
+        }
+
+        private void GraphEdit_MouseDown1(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            throw new System.NotImplementedException();
         }
 
         #endregion
 
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.MenuStrip menuStrip2;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addDSPTaskToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addDependenceToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addSubmitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exportGraphDataToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadGraphDataToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripStatusLabel statusLabel;
+        private System.Windows.Forms.ColorDialog colorDialog1;
     }
 }
 
--- GraphEdior/GraphEdit.cs
+++ GraphEdior/GraphEdit.cs
@@ -12,9 +12,67 @@ namespace GraphEdior
 {
     public partial class GraphEdit : Form
     {
+        private Point m_stCurrentPoint;
+        private Point m_stStartPoint;
+        private Graphics m_stGraphs;
+        private Pen m_stPen;
+        private List<TaskInfo> m_vTaskList = null;
+     
         public GraphEdit()
         {
             InitializeComponent();
+            this.WindowState = FormWindowState.Maximized;
+            this.m_stGraphs = this.CreateGraphics();//实例化Graphics类
+            this.m_stPen = new Pen(Color.Red, 3);//实例化Pen类
+            this.m_vTaskList = new List<TaskInfo>();
+        }
+
+        private void addDSPTaskToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void GraphEdit_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void GraphEdit_MouseMove(object sender, MouseEventArgs e)
+        {
+            m_stCurrentPoint = e.Location;
+            statusLabel.Text = m_stCurrentPoint.ToString();
+        }
+
+        private void GraphEdit_MouseDown(object sender, MouseEventArgs e)
+        {
+            m_stStartPoint = e.Location;
+        }
+
+        private void GraphEdit_MouseUp(object sender, MouseEventArgs e)
+        {
+            TaskInfo taskInfo = new TaskInfo(m_stStartPoint, e.Location);
+            m_vTaskList.Add(taskInfo);
+            ShowTask(taskInfo);
+        }
+
+        private void ShowTask(TaskInfo taskInfo)
+        {
+            m_stGraphs.DrawRectangle(m_stPen, taskInfo.X, taskInfo.Y, taskInfo.W, taskInfo.H);
+        }
+
+        private void ShowAllTasks()
+        {
+            foreach(TaskInfo taskInfo in m_vTaskList)
+            {
+                ShowTask(taskInfo);
+            }
+        }
+
+        private void GraphEdit_Resize(object sender, System.EventArgs e)
+        {
+            if (m_vTaskList != null)
+            {
+                ShowAllTasks();
+            }
         }
     }
 }
